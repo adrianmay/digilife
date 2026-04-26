321e7b79432736593481f287b645ce40af6f13d3
Adrian May <adrian.alexander.may@gmail.com>
2026-04-26 14:35:18 +0100

    Two worker threads being interrupted independently

--- hive/test/perf.c
+++ hive/test/perf.c
@@ -11,79 +11,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
+#include <time.h>
+#include "types.h"
+#include "misc/h.h"
 
-static int perf_fd;
-
-static long
-perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
-                int cpu, int group_fd, unsigned long flags)
-{
-    return syscall(__NR_perf_event_open, hw_event, pid, cpu,
-                   group_fd, flags);
-}
+int fd1;
+int fd2;
 
 long periods[] = {1000000000, 2000000000, 500000000};
 int idx = 0;
 
 void handler(int signo, siginfo_t *info, void *ucontext)
 {
-    printf("Iteration %d reached\n", idx);
+  printf("Iteration %d. told fd=%d pid=%d\n", idx, info->si_fd, info->si_value.sival_int);
 
-    idx = (idx + 1) % 3;
-
-    // stop counter
-    ioctl(perf_fd, PERF_EVENT_IOC_DISABLE, 0);
-
-    // set next period
-    long next = periods[idx];
-    ioctl(perf_fd, PERF_EVENT_IOC_PERIOD, &next);
-
-    // reset + re-arm
-    ioctl(perf_fd, PERF_EVENT_IOC_RESET, 0);
-    ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, 0);
+  //    idx = (idx + 1) % 3;
+  //    ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+  //    long next = periods[idx];
+  //    ioctl(fd, PERF_EVENT_IOC_PERIOD, &next);
+  //    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+  //    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
 }
 
-int perf()
+bool init() {
+  struct sigaction sa = {0};
+  sa.sa_sigaction = handler;
+  sa.sa_flags = SA_SIGINFO;
+  sigaction(SIGIO, &sa, NULL);
+  return true;
+}
+
+typedef struct {int secs; int fd;} go;
+
+void * monitor(void * p)
 {
-    struct perf_event_attr pe;
-    memset(&pe, 0, sizeof(pe));
+  go * pGo = (go *) p;
 
-    pe.type = PERF_TYPE_HARDWARE;
-    pe.size = sizeof(pe);
-    pe.config = PERF_COUNT_HW_CPU_CYCLES;
+  struct perf_event_attr pe;
+  memset(&pe, 0, sizeof(pe));
 
-    pe.sample_period = 200000000; // threshold
-    pe.disabled = 1;
-    pe.exclude_kernel = 0;
-    pe.exclude_hv = 1;
-    pe.pinned = 1;
+  pe.type = PERF_TYPE_HARDWARE;
+  pe.size = sizeof(pe);
+  pe.config = PERF_COUNT_HW_CPU_CYCLES;
 
-    pid_t pid = getpid(); //Whole process
-    //pid_t pid = 0;        //This thread
+  pe.sample_period = 4000000000 * pGo->secs; // threshold
+  pe.disabled = 1;
+  pe.exclude_kernel = 0;
+  pe.exclude_hv = 1;
+  pe.pinned = 1;
 
-    perf_fd = perf_event_open(&pe, pid, -1, -1, 0);
-    if (perf_fd == -1) {
-        perror("perf_event_open");
-        exit(EXIT_FAILURE);
-    }
+  pid_t pid = true ? 0 : getpid(); //this thread (0) or cpu
 
-    fcntl(perf_fd, F_SETFL, O_RDWR | O_NONBLOCK | O_ASYNC);
-    fcntl(perf_fd, F_SETSIG, SIGIO);
-    fcntl(perf_fd, F_SETOWN, getpid());
+  pGo->fd = syscall(__NR_perf_event_open, &pe, pid, -1, -1, 0);
+  if (pGo->fd == -1) {
+    perror("perf_event_open");
+    exit(EXIT_FAILURE);
+  }
 
-    struct sigaction sa = {0};
-    sa.sa_sigaction = handler;
-    sa.sa_flags = SA_SIGINFO;
-    sigaction(SIGIO, &sa, NULL);
+  fcntl(pGo->fd, F_SETFL, O_RDWR | O_NONBLOCK | O_ASYNC);
+  fcntl(pGo->fd, F_SETSIG, SIGIO);
+  fcntl(pGo->fd, F_SETOWN, getpid());
 
-    ioctl(perf_fd, PERF_EVENT_IOC_RESET, 0);
-    ioctl(perf_fd, PERF_EVENT_IOC_REFRESH, 1);
-    ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, 0);
 
-    while (1) {
-        asm volatile("" ::: "memory");
-    }
+  ioctl(pGo->fd, PERF_EVENT_IOC_RESET, 0);
+  ioctl(pGo->fd, PERF_EVENT_IOC_REFRESH, 1);
+  ioctl(pGo->fd, PERF_EVENT_IOC_ENABLE, 0);
 
-    return 0;
+  while (1) {
+    asm volatile("" ::: "memory");
+  }
 }
 
+go go1 = {2, 0};
+go go2 = {4, 0};
+
+bool perf() {
+  init();
+  pthread_t pid;
+  pthread_create(&pid, 0, monitor, &go1);
+  printf("Started thread: %ld\n", pid);
+  pthread_create(&pid, 0, monitor, &go2);
+  printf("Started thread: %ld\n", pid);
+  pthread_join(pid, 0);
+  return true;
+}
+
+// void * waitAndTalk(void * p) {
+//   long i = (long) p;
+//   Nanosecs ns = 1000000000 * i;
+//   struct timespec ts;
+//   nsToTs(ns, &ts);
+//   printf("Should wait %ld s and %'ld ns\n", ts.tv_sec, ts.tv_nsec);
+//   clock_nanosleep(CLOCK_THREAD_CPUTIME_ID, 0, &ts, 0);
+//   printf("waited %lds\n", i);
+//   return 0;
+// }
+// 
+// bool perf1() {
+//   pthread_t pid;
+//   for (long a=0;a<10;a++) {pthread_create(&pid, 0, waitAndTalk, (void *)a);}
+//   return true;
+// }

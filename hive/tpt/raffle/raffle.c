8140f3f0207a0e6e331ffbccc85429d4f00c4e7f
Adrian May <adrian.alexander.may@gmail.com>
2026-04-30 08:42:49 +0100

    Comment out future functions

--- hive/tpt/raffle/raffle.c
+++ hive/tpt/raffle/raffle.c
@@ -3,9 +3,10 @@
 #include "XX_hotel/Bulk.h"
 #include "XXBulk_pile/2.h"
 #include "XX_hotel/h.h"
-static XXBulkIndex parent(XXBulkIndex i) {return ( XXBulkIndex ){ (i.i-1)/2 };}
-static XXBulkIndex left  (XXBulkIndex i) {return ( XXBulkIndex ){ 2*i.i + 1 };}
-static XXBulkIndex right (XXBulkIndex i) {return ( XXBulkIndex ){ 2*i.i + 2 };}
+
+//static XXBulkIndex parent(XXBulkIndex i) {return ( XXBulkIndex ){ (i.i-1)/2 };}
+//static XXBulkIndex left  (XXBulkIndex i) {return ( XXBulkIndex ){ 2*i.i + 1 };}
+//static XXBulkIndex right (XXBulkIndex i) {return ( XXBulkIndex ){ 2*i.i + 2 };}
 
 XXBulkIndex enter(Cash cash, Weight w, XXTicket * pTicket) {
   XXBulk * pBulk;

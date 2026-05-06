a329c68bafbab5578af6a68f4cc5717c775b6b9c
Adrian May <adrian.alexander.may@gmail.com>
2026-05-06 17:12:35 +0100

    Oops, forgot files

---
+++ hive/lit/Mess_raffle/MessTicket.c
@@ -0,0 +1,7 @@
+#include "types.h"
+#include "MessTicket.h"
+
+void showMessTicket(MessTicket * p) {
+  printf("name=%s\n", p->name);
+
+}
---
+++ hive/lit/Mess_raffle/MessTicket.h
@@ -0,0 +1,10 @@
+#ifndef MessTicket_h
+#define MessTicket_h
+
+typedef struct {
+  char name[2];
+} MessTicket;
+
+void showMessTicket(MessTicket * p);
+
+#endif  

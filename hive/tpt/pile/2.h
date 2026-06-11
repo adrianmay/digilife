0e0de203b937cef6a41861c0d7cd8202e2f4211a
Adrian May <adrian.alexander.may@gmail.com>
2026-06-11 16:37:30 +0100

    Wip

--- hive/tpt/pile/2.h
+++ hive/tpt/pile/2.h
@@ -7,15 +7,15 @@ typedef void * (*F_XX)(XX * p, void * u);
 typedef void (*XXPileAction)(XXIx);
 
 typedef bool    (*XXPileOpen)(void);
-typedef XXIx (*XXPileAlloc)(XX ** pNew, bool * pRecycled);
+typedef XXIx    (*XXPileAlloc)(XX ** pNew, bool * pRecycled);
 typedef XX *    (*XXPileGet)(XXIx i);
-typedef void *  (*XXPileWith)(XXIx i, F_XX f, void * u);
+//typedef void *  (*XXPileWith)(XXIx i, F_XX f, void * u);
 typedef void    (*XXPileFree)(XXIx i);
 typedef void    (*XXPileClose)(FATE fate);
 typedef bool    (*XXPileIxValid)(XXIx i);
-typedef Ix   (*XXPileCount)(void);
+typedef Ix      (*XXPileCount)(void);
 // Types are same but this saves collisions:
-typedef Ix   (*XXGetUsr)(void); // Misc number
+typedef Ix      (*XXGetUsr)(void); // Misc number
 typedef void    (*XXSetUsr)(Ix u);
 typedef void    (*XXModUsr)(IxDiff u);
 typedef void    (*XXPileForAll)(bool onlyToUsr, XXPileAction action); // cos of meap
@@ -25,7 +25,7 @@ typedef struct {
   XXPileOpen open;
   XXPileAlloc alloc;
   XXPileGet get;
-  XXPileWith with;
+//  XXPileWith with;
   XXPileFree free;
   XXPileClose close;
   XXPileIxValid indexValid;
